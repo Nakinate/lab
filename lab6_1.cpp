9c0cc7b26475d9dcea3dff3ee21921f69bb19153
Nakinate Buatip <nakinate1710@gmail.com>
2021-11-26 15:17:50 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,29 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int i=1;
+    int x[i];
+    
+    while (x[i-1]!=0)
+    { 
+        cout << "Enter an integer: ";
+        cin >> x[i];
+        i++;
+    }
+    int y =0 ,z =0,q =0;
+    while (y < i-1)
+    {
+        if (x[y] %2==0)
+        {
+           z++ ;
+        }else{
+            q++;
+        }
+       
+        y++ ;
+    }
+   
+    cout << "#Even numbers = "<<z-1;
+    cout << "\n#Odd numbers = "<<q;
     return 0;
 }
